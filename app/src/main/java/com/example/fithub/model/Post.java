305a5ae51955f9b8c9cb16faa2ecf147430a5f12
0xRizki <rizkiabdelhadi4@gmail.com>
2025-12-19 21:08:25 +0100

    add Post.java in Model for Post mais rah mazal makhdamx

--- app/src/main/java/com/example/fithub/model/Post.java
+++ app/src/main/java/com/example/fithub/model/Post.java
@@ -1,4 +1,27 @@
 package com.example.fithub.model;
 
 public class Post {
+
+    private String postId;
+    private String userId;
+    private String username;
+    private String content;
+    private long timestamp;
+
+    // Obligatoire pour Firebase
+    public Post() {}
+
+    public Post(String postId, String userId, String username, String content, long timestamp) {
+        this.postId = postId;
+        this.userId = userId;
+        this.username = username;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public String getPostId() { return postId; }
+    public String getUserId() { return userId; }
+    public String getUsername() { return username; }
+    public String getContent() { return content; }
+    public long getTimestamp() { return timestamp; }
 }
